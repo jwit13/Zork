03de8fb553c52963e82241fddc4885b70b4232b6
Jess <jessica.withers13@gmail.com>
2020-12-11 00:20:20 -0500

    1.3

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -8,9 +8,41 @@ namespace Zork
         {
             Console.WriteLine("Welcome to Zork!");
 
-            string inputString = Console.ReadLine();
-            Commands command = ToCommand(inputString.Trim().ToUpper());
-            Console.WriteLine(command);
+            Commands command = Commands.UNKNOWN;
+            while (command != Commands.QUIT)
+            {
+                Console.Write("> ");
+                command = ToCommand(Console.ReadLine().Trim());
+
+                string outputString;
+
+                switch (command)
+                {
+                    case Commands.LOOK:
+                        outputString = "A rubber mat saying 'Welcome to Zork!' lies by the door.";
+                        break;
+                    case Commands.NORTH:
+                        outputString = "You moved NORTH.";
+                        break;
+                    case Commands.SOUTH:
+                        outputString = "You moved SOUTH.";
+                        break;
+                    case Commands.EAST:
+                        outputString = "You moved EAST.";
+                        break;
+                    case Commands.WEST:
+                        outputString = "You moved WEST.";
+                        break;
+                    case Commands.QUIT:
+                        outputString = "Thank you for playing!";
+                        break;
+                    default:
+                        outputString = "Unknown command.";
+                        break;
+                }
+
+                Console.WriteLine(outputString);
+            }
         }
 
         private static Commands ToCommand(string commandString)
