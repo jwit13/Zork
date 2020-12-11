03f2747fd4319329580c9fda97aeb57b243406bc
Jess <jessica.withers13@gmail.com>
2020-12-11 00:05:27 -0500

    1.1 + 1.2

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -7,6 +7,15 @@ namespace Zork
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
+
+            string inputString = Console.ReadLine();
+            Commands command = ToCommand(inputString.Trim().ToUpper());
+            Console.WriteLine(command);
+        }
+
+        private static Commands ToCommand(string commandString)
+        {
+            return Enum.TryParse<Commands>(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
         }
     }
 }
